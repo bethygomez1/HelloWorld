56a8bdb3445ec30c10f4376b5949d192ebcf9567
Gomez, Bethy <bethygomez@usf.edu>
2019-09-05 17:57:05 -0400

    Version 3 - Hello World

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* 
+ Author: Bethy Gomez
+ Date: 9/5/2019
+ Description: Class Project - Deliverable 1; Hello World console application
+*/
+using System;
 
 namespace HelloWorld
 {
@@ -7,8 +12,11 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             // Pause to display Hello World!
-            Console.WriteLine("Press any key to continue...");
+            
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Bethy and this is ISM 4300");
+            Console.WriteLine();
+            Console.WriteLine("Please press any key to continue...");
             Console.ReadKey(true);
         }
     }
